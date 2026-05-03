cce6dfc0439d6ee7e813954199ffeb667c44b5db
Artur <arbr@bk.ru>
2026-05-03 04:25:00 +0300

    json заполнил

--- main.py
+++ main.py
@@ -6,21 +6,7 @@ from app.overrides.override_manager import OverrideManager
 state = State()
 override_manager = OverrideManager(state)
 
-# Blender мягко
-override_manager.activate_override("blender", duration_sec=2.0)
-time.sleep(1)
 
-# Blender средне
-override_manager.activate_override("blender", duration_sec=4.0)
-time.sleep(1)
-
-# Blender сильно
-override_manager.activate_override("blender", duration_sec=7.0)
-time.sleep(1)
-
-# Или через профили из JSON
-override_manager.activate_intensity("blender", "soft")
-time.sleep(1)
-override_manager.activate_intensity("blender", "medium")
-time.sleep(1)
-override_manager.activate_intensity("blender", "strong")
+override_manager.activate_override("gobo_3_pol")
+time.sleep(6)
+override_manager.activate_override("gobo_3_pol")
